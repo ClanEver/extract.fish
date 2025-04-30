af5594848025dbe7a3df1fd887d4cabe4c64e214
ClanEver <562211524@qq.com>
2025-04-30 15:58:39 +0800

    Optimize and support Chinese

--- README.md
+++ README.md
@@ -10,7 +10,28 @@ Install with [Fisher](https://github.com/jorgebucaran/fisher):
 
 ## Usage
 
-    extract [-o|--output <path>] <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|exe|tar.bz2|tar.gz|tar.xz|lzma>
+```console
+❯ extract -h
+Usage: extract [-o|--output <path>] file
+Support format:
+  zip     rar    7z      tar.bz2  tbz2    tbz
+  tb2     tar.gz tgz     tar.xz   txz     tar.lzma
+  tlz     tar    bz2     gz       xz      lzma
+
+If no output path is specified, files will be extracted to the current directory.
+If the output file's directory doesn't exist, it will be created automatically.
+
+# 中文环境
+❯ extract -h
+用法: extract [-o|--output <路径>] 文件
+支持格式:
+  zip     rar    7z      tar.bz2  tbz2    tbz
+  tb2     tar.gz tgz     tar.xz   txz     tar.lzma
+  tlz     tar    bz2     gz       xz      lzma
+
+如果未指定输出路径，则解压到当前路径。
+如果输出文件的目录不存在，将自动创建。
+```
 
 ## Credits
 
--- functions/extract.fish
+++ functions/extract.fish
@@ -1,15 +1,49 @@
 function extract --description "Extract file"
-    set -l options 'o/output='
+    set -l options 'o/output=' 'h/help'
     argparse -n extract $options -- $argv
 
-    if test -z "$argv"
-        # display usage if no parameters given
-        echo "Usage: extract [-o|--output <path>] <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|exe|tar.bz2|tar.gz|tar.xz|lzma>"
-        return 1
+    # Check if we're in a Chinese environment
+    set -l is_chinese_env 0
+    if string match -qr "zh" $LANG; or string match -qr "zh" $LC_ALL; or string match -qr "zh" $LC_MESSAGES
+        set is_chinese_env 1
+    end
+
+    # display usage if no parameters given or help flag provided
+    if test -z "$argv" -o -n "$_flag_help"
+        if test $is_chinese_env -eq 1
+            echo "\
+用法: extract [-o|--output <路径>] 文件
+支持格式:
+  zip     rar    7z      tar.bz2  tbz2    tbz
+  tb2     tar.gz tgz     tar.xz   txz     tar.lzma
+  tlz     tar    bz2     gz       xz      lzma
+
+如果未指定输出路径，则解压到当前路径。
+如果输出文件的目录不存在，将自动创建。"
+        else
+            echo "\
+Usage: extract [-o|--output <path>] file
+Support format:
+  zip     rar    7z      tar.bz2  tbz2    tbz
+  tb2     tar.gz tgz     tar.xz   txz     tar.lzma
+  tlz     tar    bz2     gz       xz      lzma
+
+If no output path is specified, files will be extracted to the current directory.
+If the output file's directory doesn't exist, it will be created automatically."
+        end
+        if test -n "$_flag_help"
+            return 0
+        else
+            return 1
+        end
     end
 
     if not test -f "$argv"
-        echo "File does not exist: $argv" 
+        if test $is_chinese_env -eq 1
+            echo "文件不存在: $argv" 
+        else
+            echo "File does not exist: $argv" 
+        end
         return 1
     end
 
@@ -20,63 +54,60 @@ function extract --description "Extract file"
         if not test -d $output_dir
             mkdir -p $output_dir
             or begin
-                echo "Unable to create output directory: $output_dir"
+                if test $is_chinese_env -eq 1
+                    echo "无法创建输出目录: $output_dir"
+                else
+                    echo "Unable to create output directory: $output_dir"
+                end
                 return 1
             end
         end
     end
 
-    set -l file_path $argv
+    set -l file_path (realpath $argv)
     set -l file_name (basename $file_path)
 
     switch $file_path
-        case "*.tar.bz2"
-            tar xvjf $file_path -C $output_dir
-        case "*.tar.gz"
-            tar xvzf $file_path -C $output_dir
-        case "*.tar.xz"
-            tar xvJf $file_path -C $output_dir
         case "*.zip"
-            unzip $file_path -d $output_dir
+            unzip -q $file_path -d $output_dir
         case "*.rar"
-            unrar x $file_path $output_dir
-        case "*.bz2"
-            # bunzip2 can only extract in source directory, so copy to target directory and extract there
-            set -l dest_file $output_dir/$file_name:r
-            cp $file_path $output_dir/$file_name
-            bunzip2 $output_dir/$file_name
-        case "*.gz"
-            # gunzip can only extract in source directory, so copy to target directory and extract there
-            set -l dest_file $output_dir/$file_name:r
-            cp $file_path $output_dir/$file_name
-            gunzip $output_dir/$file_name
-        case "*.tar"
-            tar xvf $file_path -C $output_dir
-        case "*.tbz2"
-            tar xvjf $file_path -C $output_dir
-        case "*.tgz"
-            tar xvzf $file_path -C $output_dir
-        case "*.Z"
-            # uncompress can only extract in source directory, so copy to target directory and extract there
-            set -l dest_file $output_dir/$file_name:r
-            cp $file_path $output_dir/$file_name
-            uncompress $output_dir/$file_name
+            unrar x -inul $file_path $output_dir
         case "*.7z"
-            7z x $file_path -o$output_dir
+            7z x -bd $file_path -o$output_dir >/dev/null
+        case "*.tar.bz2" "*.tbz2" "*.tbz" "*.tb2"
+            tar xjf $file_path -C $output_dir
+        case "*.tar.gz" "*.tgz"
+            tar xzf $file_path -C $output_dir
+        case "*.tar.xz" "*.txz"
+            tar xJf $file_path -C $output_dir
+        case "*.tar.lzma" "*.tlz"
+            tar --lzma -xf $file_path -C $output_dir
+        case "*.tar"
+            tar xf $file_path -C $output_dir
+        case "*.bz2"
+            set -l output_file (string replace -r '\.bz2$' '' $file_name)
+            bunzip2 -c $file_path > $output_dir/$output_file
+        case "*.gz"
+            set -l output_file (string replace -r '\.gz$' '' $file_name)
+            gunzip -c $file_path > $output_dir/$output_file
         case "*.xz"
-            # use -k to keep original file, and specify output path
-            set -l dest_file $output_dir/(basename $file_path .xz)
-            xz -d -k -c $file_path > $dest_file
-        case "*.exe"
-            cabextract $file_path -d $output_dir
+            set -l output_file (string replace -r '\.xz$' '' $file_name)
+            xz -d -c $file_path > $output_dir/$output_file
         case "*.lzma"
-            # use -k to keep original file, and specify output path
-            set -l dest_file $output_dir/(basename $file_path .lzma)
-            lzma -d -k -c $file_path > $dest_file
+            set -l output_file (string replace -r '\.lzma$' '' $file_name)
+            xz -d -c $file_path > $output_dir/$output_file
         case "*"
-            echo "Unknown archive format: $file_path"
+            if test $is_chinese_env -eq 1
+                echo "未知的归档格式: $file_path"
+            else
+                echo "Unknown archive format: $file_path"
+            end
             return 1
     end
 
-    echo "File $file_path has been extracted to $output_dir"
+    if test $is_chinese_env -eq 1
+        echo "文件 $file_path 已被解压到 $output_dir"
+    else
+        echo "File $file_path has been extracted to $output_dir"
+    end
 end
